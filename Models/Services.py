be09d95d997f15ab3689fd0bf8ae99109b3baddc
Ty Church <tychurch.96@gmail.com>
2024-02-06 16:05:11 +1100

    structure updated

---
+++ Models/Services.py
@@ -0,0 +1,4 @@
+def load_data():
+    # replace the following line with a function that returns the actual data you wish to load
+    data = "This is the data loaded from services.py"
+    return data
--- app.py
+++ app.py
@@ -1,12 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def home():  # put application's code here
+    return render_template('index.html')
 
+@app.route('/load-data')
+def load_data():
+    # Call your function from services.py and return the data.
+    data = services.load_data()
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1',port=5050)
+
+
