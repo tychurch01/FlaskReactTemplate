83d8a000e32a27c33ab37456af75f922dcb0b104
Ty Church <tychurch.96@gmail.com>
2024-02-08 11:12:50 +1100

    react files added

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, send_from_directory
 import os
+import subprocess
 
 app = Flask(__name__, static_folder='./build')
 
@@ -12,5 +13,20 @@ def serve_data():
     # Put the data you want to send here
     return {"data": "Hello, World!"}
 
+def start_react():
+    try:
+        os.chdir('./frontend/')  # Try to navigate to my-app directory
+    except FileNotFoundError:
+        print("Could not find directory '.../frontend/my-app'. Please check that your paths are correct.")
+        return  # If we can't find the directory we should not continue trying to start the process
+
+    try:
+        subprocess.Popen("npm start", shell=True)  # Try to start the react app
+    except Exception as e:
+        print("Could not start the React app. Ensure you have npm installed and try again.")
+        print("Error:", e)
+
+
 if __name__ == '__main__':
-    app.run(use_reloader=True, debug=True, host='0.0.0.0')
+    start_react()
+    app.run(use_reloader=True, debug=True, host='0.0.0.0',port=5051)
